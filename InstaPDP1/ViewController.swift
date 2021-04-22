2e13d44b3cd6f1a0618b4751e93c5e6782ae4d5f
Muhammadjon <20mamarasulov@gmail.com>
2021-04-22 23:27:53 +0900

    Initial Commit

---
+++ InstaPDP1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaPDP1
+//
+//  Created by Muhammadjon Mamarasulov on 4/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
